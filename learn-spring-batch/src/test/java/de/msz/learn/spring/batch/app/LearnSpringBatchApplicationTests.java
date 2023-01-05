787380854fb13ec53f1ec8edf2caf1bf6b18d8dd
Mathias Schulze <mathias-schulze@web.de>
2023-01-05 21:57:53 +0100

    initial project setup with Spring Tools Suite

---
+++ learn-spring-batch/src/test/java/de/msz/learn/spring/batch/app/LearnSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package de.msz.learn.spring.batch.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
