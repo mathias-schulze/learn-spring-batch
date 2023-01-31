618d0ad68fb5fa9b39a320c3c37c60d655f8f4f7
Mathias Schulze <mathias-schulze@web.de>
2023-01-31 20:39:54 +0100

    stop job

--- learn-spring-batch/src/main/java/de/msz/learn/spring/batch/controller/JobController.java
+++ learn-spring-batch/src/main/java/de/msz/learn/spring/batch/controller/JobController.java
@@ -1,6 +1,9 @@
 package de.msz.learn.spring.batch.controller;
 
 import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobExecutionNotRunningException;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.NoSuchJobExecutionException;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
@@ -19,11 +22,26 @@ public class JobController {
 	@Autowired
 	JobService jobService;
 	
+	@Autowired
+	JobOperator jobOperator;
+	
 	@GetMapping("/start/{jobName}")
 	public String startJob(@PathVariable String jobName) throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
 		
 		jobService.startJob(jobName);
 		
 		return "Job started...";
+	}
+	
+	@GetMapping("/stop/{jobExecutionId}")
+	public String stopJob(@PathVariable long jobExecutionId) {
+		
+		try {
+			jobOperator.stop(jobExecutionId);
+		} catch (NoSuchJobExecutionException | JobExecutionNotRunningException e) {
+			e.printStackTrace();
+		}
+		
+		return "Job stopped...";
 	}
 }
